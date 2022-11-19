5bc7f70e31f73800e63ec5c73c3823aeb20a4fd8
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2022-11-19 15:16:25 +0300

    feat(layout): create layout component

--- calculator/src/app.tsx
+++ calculator/src/app.tsx
@@ -1,7 +1,20 @@
+import React from "react";
 import "./app.css";
 
+const Layout = ({ children }: React.PropsWithChildren) => {
+  return (
+    <div className="mx-auto flex h-screen flex-col items-center justify-center p-4 file:container">
+      <main className=" mb-auto h-full w-full max-w-screen-md">{children}</main>
+    </div>
+  );
+};
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Layout>
+      <div className="bg-red-100 w-full h-full"></div>
+    </Layout>
+  );
 }
 
 export default App;
