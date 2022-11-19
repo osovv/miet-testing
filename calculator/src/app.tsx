ce9f2cbae0d1097b436d42f1e7c8cd19d5a50f9d
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2022-11-19 15:48:35 +0300

    feat(ui): create basic html layout

--- calculator/src/app.tsx
+++ calculator/src/app.tsx
@@ -9,10 +9,51 @@ const Layout = ({ children }: React.PropsWithChildren) => {
   )
 }
 
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  label: string
+}
+
+const Input = ({ value, onChange, label }: InputProps) => {
+  return (
+    <>
+      <div className="flex flex-col">
+        <label className="text-xl">{label}</label>
+        <input className="border-2" value={value} onChange={onChange} />
+      </div>
+    </>
+  )
+}
+
+const Button = ({
+  children,
+  onClick,
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return (
+    <button
+      className="w-16 h-full border-2 text-center font-bold text-xl"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
+
 export const App = () => {
   return (
     <Layout>
-      <div className="bg-red-100 w-full h-full" />
+      <div className="flex flex-col lg:flex-row items-center justify-center gap-4">
+        <Input label="First argument" />
+        <Input label="Second argument" />
+      </div>
+      <div className="flex items-center justify-center mt-10 gap-4">
+        <Button>+</Button>
+        <Button>-</Button>
+        <Button>x</Button>
+        <Button>/</Button>
+      </div>
+      <div className="flex items-center justify-center mt-10 font-bold text-2xl">
+        Result: 50
+      </div>
     </Layout>
   )
 }
