7ad86769f65dbfe9ff27a2c85037a22a62f7fd1f
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2022-12-03 13:54:24 +0300

    add more tests

--- tests/tests/myshow-register.spec.ts
+++ tests/tests/myshow-register.spec.ts
@@ -1,13 +1,14 @@
 import { test, expect } from "@playwright/test";
 import { faker } from "@faker-js/faker";
 
-const username = faker.internet.userName();
+const username = faker.name.firstName() + faker.random.numeric();
 const email = faker.internet.email();
 const password = faker.internet.password();
 
-test("test", async ({ page }) => {
+test("registration", async ({ page }) => {
   await page.goto("https://myshows.me/");
-  await page.getByText("Войти").first().click();
+  await page.waitForLoadState("networkidle");
+  await page.locator(".Login-container").click();
   await page.getByRole("link", { name: "Регистрация" }).click();
   await page.getByRole("textbox").first().fill(username);
   await page.locator('input[type="email"]').fill(email);
@@ -16,4 +17,18 @@ test("test", async ({ page }) => {
   await page.getByRole("button", { name: "Войти" }).click();
 
   await page.isVisible(`Привет, ${username}`);
+});
+
+test("search", async ({ page }) => {
+  await page.goto("https://myshows.me/");
+  await page.getByPlaceholder("Рик и Морти").fill("Рик и Морти");
+  await page.getByRole("button", { name: "Найти" }).click();
+
+  await page.getByRole("link", { name: "Рик и Морти" }).isVisible();
+});
+
+test("not found page", async ({ page }) => {
+  await page.goto(`https://myshows.me/${faker.datatype.uuid()}`);
+
+  await page.getByRole("heading", { name: "Оппс… Заблудился?" }).isVisible();
 });
